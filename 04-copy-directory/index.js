c0f03237c79f2ae83881177c30cae92d93f39111
Sergey Chikunov <84094895+hapikus@users.noreply.github.com>
2023-05-05 01:13:55 +0700

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const path = require('path');
+const fs = require('fs');
+const fsPromise = require('fs/promises');
+
+let folderName = 'files';
+let folderPath = path.join(__dirname, folderName);
+let copyFolderName = 'files-copy';
+let copyFolderPath = path.join(__dirname, copyFolderName);
+
+
+fs.mkdir(copyFolderPath, {recursive: true}, () => {
+  fsPromise.readdir(folderPath)
+    .then((files) => {
+      files.forEach((file) => {
+        let filePath = path.join(folderPath, file);
+        let copyFilePath =  path.join(copyFolderPath, file);
+        fsPromise.copyFile(filePath, copyFilePath)
+          .catch(err => err);
+      });
+    });
+});
 
